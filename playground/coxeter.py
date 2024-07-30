cd9bb50ec859dfe3aa1883a50598980eef1b317a
Lukas Boelling <lukasboelling002@gmail.com>
2024-07-29 22:41:00 -0400

    Reflection matrices

--- playground/coxeter.py
+++ playground/coxeter.py
@@ -1,54 +1,109 @@
-import numpy as np
+import sympy as sp
 
-# Diagram:
-#     o2
-# 0  1|   3
-# o---o---o
 
-# Matrix:
-matrix = np.array([
-    [1, 3, 2, 2],
-    [3, 1, 3, 3],
-    [2, 3, 1, 2],
-    [2, 3, 2, 1],
-])
-N = len(matrix)
+def sixteen_cell():
+    # Diagram:
+    #     o2
+    # 0  1|   3
+    # o---o---o
 
-normals = np.array([[1, 0, 0, 0]])
+    # Matrix:
+    matrix = sp.Matrix([
+        [1, 3, 2, 2],
+        [3, 1, 3, 3],
+        [2, 3, 1, 2],
+        [2, 3, 2, 1],
+    ])
+    return matrix
+
+
+def cube():
+    matrix = sp.Matrix([
+        [1, 4, 2],
+        [4, 1, 3],
+        [2, 3, 1],
+    ])
+    return matrix
+
+
+def reflect(d, n):
+    return d - 2 * d.dot(n) * n
+
+
+def reflection_matrix(n):
+    x = reflect(sp.Matrix([1, 0, 0]), n)
+    y = reflect(sp.Matrix([0, 1, 0]), n)
+    z = reflect(sp.Matrix([0, 0, 1]), n)
+    return x.row_join(y).row_join(z)
+
+
+matrix = cube()
+N = sp.shape(matrix)[0]
+
+print(N, matrix)
+
+normals = sp.zeros(1, N)
+normals[0] = 1
 
 
 for i in range(1, N):
     subarray = normals[:i, :i]
-    b = np.cos(np.pi / matrix[i, :i])
-    print(subarray)
-    print(b)
 
-    solution = np.linalg.inv(subarray) @ b
-    print(solution)
+    b = matrix[:i, i]
+    b = b.applyfunc(lambda x: sp.cos(sp.pi / x))
 
-    new_normal = np.zeros(N)
-    new_normal[:i] = solution
+    solution = subarray.inv() * b
 
-    print(new_normal)
-    last_element = (1 - np.sum(new_normal**2)) ** 0.5
+    new_normal = sp.zeros(1, N)
+
+    for j in range(i):
+        new_normal[0, j] = solution[j]
+
+    sum_of_squares = new_normal.dot(new_normal)
+
+    last_element = sp.sqrt((1 - sum_of_squares))
+
     new_normal[i] = last_element
-    print(new_normal)
-    print(np.sum(new_normal**2))
 
-    normals = np.vstack((normals, [new_normal]))
-
-    print()
+    row_idx = normals.shape[0]
+    normals = normals.row_insert(row_idx, new_normal)
 
 
 for i in range(N):
     for j in range(i, N):
-        norm_i = normals[i]
-        norm_j = normals[j]
+        norm_i = normals[i, :]
+        norm_j = normals[j, :]
 
-        dp = norm_i @ norm_j
+        dp = norm_i.dot(norm_j)
         if dp == 1:
             coxeter_number = 1
         else:
-            coxeter_number = 1 / (np.acos(dp) / np.pi)
+            coxeter_number = 1 / (sp.acos(dp) / sp.pi)
         print((i, j), coxeter_number, matrix[i, j])
-print(normals)
+print("normals:", normals)
+
+gens = []
+for i in range(N):
+    n = normals.row(i).T
+    gens.append(reflection_matrix(n))
+
+for g in gens:
+    print(g)
+
+
+# class GroupElement:
+#     sequence: list[int]
+#     matrix: sp.Matrix
+
+#     def __init__(self, sequence: list[int], matrix: sp.Matrix):
+#         self.sequence = sequence
+#         self.matrix = matrix
+
+
+# elements = []
+# queue = [
+#     GroupElement([], sp.eye(N))
+# ]
+
+# while queue:
+
