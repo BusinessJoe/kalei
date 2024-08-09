d5223077afe941944c3d490004b02151255aeed0
Lukas Boelling <lukasboelling002@gmail.com>
2024-08-09 18:25:34 -0400

    Square free decomposition

--- src/ipoly.rs
+++ src/ipoly.rs
@@ -1,12 +1,12 @@
+use itertools::{Itertools, ZipLongest};
 use num_integer::{gcd, lcm};
 use num_rational::Rational64;
-use itertools::{Itertools, ZipLongest};
 
 type Integer = i64;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 /// Polynomial over the integers
-struct IPolynomial {
+pub struct IPolynomial {
     coefs: Vec<Integer>,
 }
 
@@ -39,22 +39,43 @@ impl IPolynomial {
     }
 }
 
+impl std::ops::Neg for IPolynomial {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        let coefs: Vec<_> = self.coefs.iter().map(|c| -c).collect();
+        Self::new(&coefs)
+    }
+}
+
 impl std::ops::Sub for IPolynomial {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        let mut coefs: Vec<_> = self.coefs.iter().zip_longest(rhs.coefs).map(|zip| {
-            match zip {
+        let mut coefs: Vec<_> = self
+            .coefs
+            .iter()
+            .zip_longest(rhs.coefs)
+            .map(|zip| match zip {
                 itertools::EitherOrBoth::Both(lc, rc) => lc - rc,
-                itertools::EitherOrBoth::Left(lc) => *lc,
+                itertools::EitherOrBoth::Left(&lc) => lc,
                 itertools::EitherOrBoth::Right(rc) => -rc,
-            }
-        }).collect();
+            })
+            .collect();
 
         while coefs.len() > 1 && Some(&0) == coefs.last() {
             coefs.pop();
         }
 
+        Self::new(&coefs)
+    }
+}
+
+impl std::ops::Mul<Integer> for IPolynomial {
+    type Output = Self;
+
+    fn mul(self, rhs: Integer) -> Self::Output {
+        let coefs: Vec<_> = self.coefs.iter().map(|c| c * rhs).collect();
         Self::new(&coefs)
     }
 }
@@ -117,12 +138,16 @@ impl std::ops::Neg for Algebraic {
 /// This algorithm only works for polynomials over fields, so we return
 /// rational coefficients instead of integral ones.
 fn euclid_div_rational(a: &[Rational64], b: &[Rational64]) -> (Vec<Rational64>, Vec<Rational64>) {
+    // Adaptation from
+    // https://en.wikipedia.org/wiki/Polynomial_greatest_common_divisor#Euclidean_division
     let mut q: Vec<Rational64> = vec![(0).into(); a.len()];
     let mut r: Vec<Rational64> = a.iter().map(|&c| c.into()).collect();
     let d = b.len() - 1;
     let c = b.last().unwrap();
 
-    while r.len() - 1 >= d {
+    // In the pseudocode, deg(0) < 0 by convention
+    // so we check if r == 0 as well here
+    while r != [Rational64::ZERO] && r.len() - 1 >= d {
         let s = r.last().unwrap() / c;
         let s_exp = r.len() - 1 - d;
         q[s_exp] += s;
@@ -148,16 +173,27 @@ fn euclid_div_rational(a: &[Rational64], b: &[Rational64]) -> (Vec<Rational64>, 
 
 /// Assumes a is divisible by b
 fn ipoly_div(a: &IPolynomial, b: &IPolynomial) -> IPolynomial {
-    let a_rat: Vec<_> = a.coefs.iter().map(|&c| Rational64::from_integer(c)).collect();
-    let b_rat: Vec<_> = b.coefs.iter().map(|&c| Rational64::from_integer(c)).collect();
+    let a_rat: Vec<_> = a
+        .coefs
+        .iter()
+        .map(|&c| Rational64::from_integer(c))
+        .collect();
+    let b_rat: Vec<_> = b
+        .coefs
+        .iter()
+        .map(|&c| Rational64::from_integer(c))
+        .collect();
 
     // Assume that the remainder is 0
     let (q, _) = euclid_div_rational(&a_rat, &b_rat);
 
-    let coefs: Vec<_> = q.into_iter().map(|c| {
-        debug_assert!(c.is_integer());
-        *c.numer()
-    }).collect();
+    let coefs: Vec<_> = q
+        .into_iter()
+        .map(|c| {
+            debug_assert!(c.is_integer());
+            *c.numer()
+        })
+        .collect();
 
     IPolynomial::new(&coefs)
 }
@@ -176,14 +212,33 @@ fn euclid_gcd_rational(a: &[Rational64], b: &[Rational64]) -> Vec<Rational64> {
 
 /// Returns the GCD of two polynomials.
 /// Implemented via Euclidean algorithm.
-fn euclid_gcd(a: &IPolynomial, b: &IPolynomial) -> IPolynomial {
+pub fn euclid_gcd(a: &IPolynomial, b: &IPolynomial) -> IPolynomial {
+    if a.coefs == [0] && b.coefs == [0] {
+        return a.clone();
+    }
+
+    // Find gcd of all coefficients
+    let mut gcd_all_coefs = a.coefs[0];
+    for c in &a.coefs {
+        gcd_all_coefs = gcd(gcd_all_coefs, *c);
+    }
+    for c in &b.coefs {
+        gcd_all_coefs = gcd(gcd_all_coefs, *c);
+    }
+
     // Extend to rationals for gcd algorithm
-    let a_rat: Vec<_> = a.coefs.iter().map(|&c| Rational64::from_integer(c)).collect();
-    let b_rat: Vec<_> = b.coefs.iter().map(|&c| Rational64::from_integer(c)).collect();
+    let a_rat: Vec<_> = a
+        .coefs
+        .iter()
+        .map(|&c| Rational64::from_integer(c))
+        .collect();
+    let b_rat: Vec<_> = b
+        .coefs
+        .iter()
+        .map(|&c| Rational64::from_integer(c))
+        .collect();
 
     let gcd_rat = euclid_gcd_rational(&a_rat, &b_rat);
-
-    dbg!(&gcd_rat);
 
     // Find lcm of denominators
     let mut denom_lcm = 1;
@@ -191,11 +246,14 @@ fn euclid_gcd(a: &IPolynomial, b: &IPolynomial) -> IPolynomial {
         denom_lcm = lcm(denom_lcm, *c.denom());
     }
 
-    let mut coefs: Vec<_> = gcd_rat.into_iter().map(|mut coef| {
-        coef *= denom_lcm;
-        debug_assert!(coef.is_integer());
-        *coef.numer()
-    }).collect();
+    let mut coefs: Vec<_> = gcd_rat
+        .into_iter()
+        .map(|mut coef| {
+            coef *= denom_lcm;
+            debug_assert!(coef.is_integer());
+            *coef.numer()
+        })
+        .collect();
 
     // Divide coefs by gcd
     let mut coef_gcd = coefs[0];
@@ -204,36 +262,50 @@ fn euclid_gcd(a: &IPolynomial, b: &IPolynomial) -> IPolynomial {
     }
 
     coefs.iter_mut().for_each(|c| *c /= coef_gcd);
-    IPolynomial::new(&coefs)
+    let poly = IPolynomial::new(&coefs) * gcd_all_coefs;
+    if poly.coefs.last().unwrap() < &0 {
+        -poly
+    } else {
+        poly
+    }
 }
-
 
 /// Returns the square free decomposition of the given polynomial.
 /// Implemented via Yun's algorithm.
-fn square_free_decomposition(f: &IPolynomial) -> Vec<IPolynomial> {
+fn square_free_decomposition(f: &IPolynomial) -> Option<Vec<IPolynomial>> {
+    // Yun's algorithm works for primitive polynomials - we fail if the GCD of coefficients is
+    // greater than 1.
+    let mut coef_gcd = f.coefs[0];
+    for c in &f.coefs {
+        coef_gcd = gcd(coef_gcd, *c);
+    }
+    if coef_gcd != 1 {
+        return None;
+    }
+
     let f_prime = f.derivative();
 
-    let mut outputs: Vec<IPolynomial> = vec![
-        euclid_gcd(&f, &f_prime)
-    ];
+    let a_0 = euclid_gcd(&f, &f_prime);
+    dbg!(&a_0);
+    let mut outputs: Vec<IPolynomial> = vec![];
 
-    let mut b = ipoly_div(&f, &outputs[0]);
-    let mut c = ipoly_div(&f_prime, &outputs[0]);
+    let mut b = ipoly_div(&f, &a_0);
+    let mut c = ipoly_div(&f_prime, &a_0);
     let mut d = c - b.derivative();
-    let mut iter = 0;
 
-    while b.coefs != [1] {
-        dbg!(&b, &d);
-        panic!("bruh");
+    loop {
         let a = euclid_gcd(&b, &d);
+        dbg!(&a);
         b = ipoly_div(&b, &a);
         c = ipoly_div(&d, &a);
         d = c - b.derivative();
         outputs.push(a);
-        iter += 1;
+        if b.coefs == [1] {
+            break;
+        }
     }
 
-    outputs
+    Some(outputs)
 }
 
 #[cfg(test)]
@@ -278,7 +350,7 @@ mod tests {
 
         let gcd = euclid_gcd(&a, &b);
         dbg!(&gcd);
-        
+
         assert_eq!(gcd.coefs, [1]);
     }
 
@@ -291,7 +363,7 @@ mod tests {
 
         let gcd = euclid_gcd(&a, &b);
         dbg!(&gcd);
-        
+
         assert_eq!(gcd.coefs, [1, 1]);
     }
 
@@ -304,23 +376,109 @@ mod tests {
 
         let gcd = euclid_gcd(&a, &b);
         dbg!(&gcd);
-        
+
         assert_eq!(gcd.coefs, [2, 1, 1]);
     }
 
     #[test]
-    fn test_derivative() {
-        // x^2 -> 2x
-        let a = IPolynomial::new(&[0, 0, 1]);
+    fn euclid_gcd_contsnts() {
+        let a = IPolynomial::new(&[2]);
+        let b = IPolynomial::new(&[0]);
+
+        let gcd = euclid_gcd(&a, &b);
+        assert_eq!(gcd.coefs, [2]);
+    }
+
+    #[test]
+    fn test_derivative_moic() {
+        // x^2 + 1 -> 2x
+        let a = IPolynomial::new(&[1, 0, 1]);
 
         assert_eq!(a.derivative(), IPolynomial::new(&[0, 2]))
     }
 
     #[test]
-    fn test_square_free_decomposition() {
-        // x^2 -> x
+    fn derivative_non_monic() {
+        // 2x^2 - 18 -> 4x
+        let a = IPolynomial::new(&[-18, 0, 2]);
+        assert_eq!(a.derivative(), IPolynomial::new(&[0, 4]));
+    }
+
+    #[test]
+    fn square_free_decomposition_x_squared() {
+        // x^2 = 1 * (x)^2
         let a = IPolynomial::new(&[0, 0, 1]);
         let square_free = square_free_decomposition(&a);
-        assert_eq!(square_free, vec![IPolynomial::new(&[0, 1])])
+        assert_eq!(
+            square_free,
+            Some(vec![IPolynomial::new(&[1]), IPolynomial::new(&[0, 1])])
+        )
+    }
+
+    #[test]
+    fn square_free_decomposition_x_minus_1_squared() {
+        // x^2 - 2x + 1 = 1 * (x - 1)^2
+        let a = IPolynomial::new(&[1, -2, 1]);
+        let square_free = square_free_decomposition(&a);
+        assert_eq!(
+            square_free,
+            Some(vec![IPolynomial::new(&[1]), IPolynomial::new(&[-1, 1])])
+        )
+    }
+
+    #[test]
+    fn square_free_decomposition_non_primitive() {
+        // Polynomial is not primitive
+        // 2x^2 - 18 -> 2x^2 - 18
+        let a = IPolynomial::new(&[-18, 0, 2]);
+        let square_free = square_free_decomposition(&a);
+        assert_eq!(square_free, None)
+    }
+
+    #[test]
+    fn square_free_decomposition_already_square_free() {
+        // Polynomial is already square free so we expect
+        // 2x^2 - 9 -> 2x^2 - 9
+        let a = IPolynomial::new(&[-9, 0, 2]);
+        let square_free = square_free_decomposition(&a);
+        assert_eq!(square_free, Some(vec![a]))
+    }
+
+    #[test]
+    fn square_free_decomposition_complicated() {
+        // (x - 1)^3 (2x + 5)^7 (x + 1) (x + 2)
+        let square_free = square_free_decomposition(&IPolynomial::new(&[
+            -156250, -203125, 287500, 562500, 35000, -445475, -295330, 22564, 112664, 61296, 16288,
+            2240, 128,
+        ]));
+        assert_eq!(
+            square_free,
+            Some(vec![
+                IPolynomial::new(&[2, 3, 1]),
+                IPolynomial::new(&[1]),
+                IPolynomial::new(&[-1, 1]),
+                IPolynomial::new(&[1]),
+                IPolynomial::new(&[1]),
+                IPolynomial::new(&[1]),
+                IPolynomial::new(&[5, 2]),
+            ])
+        )
+    }
+
+    #[test]
+    fn square_free_decomposition_no_roots() {
+        // (x^2 + 1)^2 (x^2 - x + 1)^3
+        let square_free = square_free_decomposition(&IPolynomial::new(&[
+            // x^10 - 3 x^9 + 8 x^8 - 13 x^7 + 19 x^6 - 20 x^5 + 19 x^4 - 13 x^3 + 8 x^2 - 3 x + 1
+            1, -3, 8, -13, 19, -20, 19, -13, 8, -3, 1
+        ]));
+        assert_eq!(
+            square_free,
+            Some(vec![
+                IPolynomial::new(&[1]),
+                IPolynomial::new(&[1, 0, 1]),
+                IPolynomial::new(&[1, -1, 1]),
+            ])
+        )
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use kalei::poly::Polynomial;
+use kalei::ipoly::{IPolynomial, euclid_gcd};
 
 fn main() {
-    let p = Polynomial::new(&[(1).into(), (-4).into(), (-4).into(), (8).into()]);
+    let a = IPolynomial::new(&[-9, 0, 2]);
+    let b = IPolynomial::new(&[0, 4]);
+    let gcd = euclid_gcd(&a, &b);
 
-    for loc in p.real_root_locations() {
-        println!("{}", loc);
-    }
+    dbg!(gcd);
 }
